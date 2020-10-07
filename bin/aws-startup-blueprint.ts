3ea8ffe91049aa9822972e5ac94b269d71643e4c
Paul Underwood <paulu@amazon.com>
2020-10-07 15:51:43 -0500

    Removing files from doc branch

--- bin/aws-startup-blueprint.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { AwsStartupBlueprintStack } from '../lib/aws-startup-blueprint-stack';
-
-const app = new cdk.App();
-new AwsStartupBlueprintStack(app, 'AwsBiotechBlueprint', {
-    description: "The AWS Biotech Blueprint is a strongly opinionated architecture for any Startup looking to start using AWS follwing best practices on day 1. (qs-1of009lua)"
-});
